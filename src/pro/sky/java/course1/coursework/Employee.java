51d9d9cb148935dcd268b05350511e9efcf40e80
admin <promitay@gmail.com>
2022-06-16 23:03:22 +0300

    Курсовая работа, Повышенная сложность

--- src/pro/sky/java/course1/coursework/Employee.java
+++ src/pro/sky/java/course1/coursework/Employee.java
@@ -51,6 +51,19 @@ public class Employee {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
     public String toString() {
         return "{" +
                 "id = " + id +
